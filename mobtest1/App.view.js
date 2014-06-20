cf9818df83d431b837b22a4bc63c62d0c05a7fc6
evrkgithub <evrk.sap@gmail.com>
2014-06-20 16:13:14 +0530

    First Commit

---
+++ mobtest1/App.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("mobtest1.App", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf mobtest1.App
+	*/ 
+	getControllerName : function() {
+		return "mobtest1.App";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf mobtest1.App
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
